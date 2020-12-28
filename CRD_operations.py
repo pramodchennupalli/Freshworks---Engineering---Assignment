110e0ca592e0c6e517d9b8caffb309c4ddf0dc6b
pramodchennupalli <pramodchennupalli@gmail.com>
2020-12-28 10:08:52 +0530

    #Create Operation

--- CRD_operations.py
+++ CRD_operations.py
@@ -1,1 +1,17 @@
 dict = {}
+
+def create(key, value, timeout=0):
+    if key in dict:
+        print('error: This key already exists')
+    else:
+        if len(dict) < pow(1024, 3) and value <= 16 * pow(1024, 2):
+            if(key.isalpha()):
+                if timeout == 0:
+                    li = [value, timeout]
+                else:
+                    li = [value, time.time() + timeout]
+            else:
+                print('error: Invalid Key (Note: key must contain only alphabet)')
+        else:
+            print('error: Memory limit exceeded')
+

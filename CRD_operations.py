fff049b6c6ad9921fe57d0179e22d938aebc9222
pramodchennupalli <pramodchennupalli@gmail.com>
2020-12-28 11:39:43 +0530

    Final

--- CRD_operations.py
+++ CRD_operations.py
@@ -1,19 +1,20 @@
-import threading
 import time
-from threading import *
 
-dict = {}
+dic = {}
+
 
 def create(key, value, timeout=0):
-    if key in dict:
+    if key in dic:
         print('error: This key already exists')
     else:
-        if len(dict) < pow(1024, 3) and value <= 16 * pow(1024, 2):
-            if(key.isalpha()):
+        if len(dic) < pow(1024, 3) and value <= 16 * pow(1024, 2):
+            if key.isalpha():
                 if timeout == 0:
                     li = [value, timeout]
                 else:
                     li = [value, time.time() + timeout]
+                if len(key) <= 32:
+                    dic[key] = li
             else:
                 print('error: Invalid Key (Note: key must contain only alphabet)')
         else:
@@ -21,27 +22,27 @@ def create(key, value, timeout=0):
 
 
 def read(key):
-    if key not in dict:
+    if key not in dic:
         print('error: Given key not found. Enter valid Key')
     else:
-        p = dict[key]
+        p = dic[key]
         if p[1] != 0:
             if time.time() < p[1]:
-                return str(key)+":"+str(p[0])
+                return str(key) + ":" + str(p[0])
             else:
                 print('error: time-to-live of', key, 'has expired')
         else:
-            return str(key)+':'+str(p[0])
+            return str(key) + ':' + str(p[0])
 
 
 def delete(key):
-    if key not in dict:
+    if key not in dic:
         print('error: Given key not found. Enter valid Key')
     else:
-        p = dict[key]
+        p = dic[key]
         if p[1] != 0:
             if time.time() < p[1]:
-                del dict[key]
+                del dic[key]
                 print('Deleted Successfully !!')
             else:
                 print('error: time-to-live of', key, 'has expired')
--- unit_test.py
+++ unit_test.py
@@ -1,20 +1,23 @@
+import time
+from threading import *
+
 import CRD_operations as crd
 
-crd.create('pramod', 77) #with no time-to-live property
+crd.create('pramod', 77)  # with no time-to-live property
 
-crd.create('bhaskar', 16, 3600) #with time-to-live property
+crd.create('bhaskar', 16, 3600)  # with time-to-live property
 
-crd.read('pramod') #returns the value for respective key
+print(crd.read('pramod'))  # returns the value for respective key
 
-crd.read('bhaskar') #return the value for respective key
+print(crd.read('bhaskar'))  # return the value for respective key
 
-crd.delete('pro') #return error because key not existed in database
+crd.delete('pro')  # return error because key not existed in database
 
-crd.delete('bhaskar') #return delete respective key from database
+crd.delete('bhaskar')  # return delete respective key from database
 
-t1 = Thread(target = (create or read or delete), args = (key, value, timeout)) #as per the operation
+t1 = Thread(target=(crd.create or crd.read or crd.delete))  # as per the operation
 t1.start()
-t1.sleep()
-t2 = Thread(target = (create or read or delete), args = (key, value, timeout)) #as per the operation
+time.sleep(1)
+t2 = Thread(target=(crd.create or crd.read or crd.delete))  # as per the operation
 t2.start()
-t2.sleep()
+time.sleep(1)
